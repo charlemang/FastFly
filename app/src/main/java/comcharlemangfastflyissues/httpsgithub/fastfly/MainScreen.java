836508f8d75954cf576ad697b3f5cf129ac352f6
abhikante <akante@ufl.edu>
2018-10-20 03:05:44 -0400

    Linked home and main screens

---
+++ app/src/main/java/comcharlemangfastflyissues/httpsgithub/fastfly/MainScreen.java
@@ -0,0 +1,13 @@
+package comcharlemangfastflyissues.httpsgithub.fastfly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
