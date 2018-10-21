431d15c92aedd1c11709dbdbf142525b1fd66cc4
abhikante <akante@ufl.edu>
2018-10-21 01:32:41 -0400

    Some tweaking

--- app/src/main/java/comcharlemangfastflyissues/httpsgithub/fastfly/AirportView.java
+++ app/src/main/java/comcharlemangfastflyissues/httpsgithub/fastfly/AirportView.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -20,6 +21,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class AirportView extends FragmentActivity implements OnMapReadyCallback {
 
@@ -27,15 +30,27 @@ public class AirportView extends FragmentActivity implements OnMapReadyCallback 
     private LocationManager locationManager;
     private Location location;
     private final int LOCATION_PERMISSION_CODE = 1;
+    private String airportName;
+    private char terminal;
+    private int gateNumber;
+    private String flightTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airport_view);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Intent intent = getIntent();
+        airportName = intent.getStringExtra(MainScreen.EXTRA_AIRPORT);
+        String gate = intent.getStringExtra(HomeScreen.EXTRA_GATE);
+        terminal = gate.charAt(0);
+        gateNumber = Integer.parseInt(gate.substring(1,3));
+        flightTime = intent.getStringExtra(HomeScreen.EXTRA_TIME);
     }
 
 
@@ -63,8 +78,29 @@ public class AirportView extends FragmentActivity implements OnMapReadyCallback 
         double longitude = location.getLongitude();
         double latitude = location.getLatitude();
 
+        LatLng center; // = new LatLng(33.640775, -84.433333);
+        Polyline line = mMap.addPolyline(new PolylineOptions().add(new LatLng(latitude, longitude) ))
+
+        switch(terminal) {
+            case 'A':
+                center = new LatLng(33.6408608, -84.4391428);
+
+            case 'B':
+                center = new LatLng(33.64071, -84.43591);
+            case 'C':
+                center = new LatLng(33.64066, -84.43259);
+            case 'D':
+                center = new LatLng(33.64061, -84.42929);
+            case 'E':
+                center = new LatLng(33.6406,-84.42577);
+        }
+
+
+
+
+
+
         LatLngBounds ATLAirport = new LatLngBounds(new LatLng(33.637194, -84.446728), new LatLng(33.644034,-84.417289));
-        LatLng loc = new LatLng(33.640775, -84.433333);
         mMap.addMarker(new MarkerOptions().position(loc).title("Current Location"));
         mMap.setMinZoomPreference(17);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 19));
